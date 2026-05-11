8ffca3107af67f925f7d4055202338459ee97edb
WIZARD OF OZ <nurajabegum81@gmail.com>
2026-05-12 01:57:30 +0530

    Config file added for Music player

---
+++ src/data/songs.ts
@@ -0,0 +1,16 @@
+export const FEATURED_SONGS = [
+  {
+    id: "youtube_video_id",
+    title: "Perfect",
+    artist: "Ed Sheeran",
+  },
+  {
+    id: "youtube_video_id",
+    title: "All of Me",
+    artist: "John Legend",
+  },
+];
+
+export const PLAYLIST_SONGS = [
+  // more songs here
+];
