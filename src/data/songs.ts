20ee93b13ff4585a3ba0e3e6a76da6beb8a69965
WIZARD OF OZ <nurajabegum81@gmail.com>
2026-05-12 21:34:35 +0530

    Added a few songs for the music player

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -1,16 +1,135 @@
 export const FEATURED_SONGS = [
   {
-    id: "youtube_video_id",
-    title: "Perfect",
-    artist: "Ed Sheeran",
+    id: "fukGbiPuBjU",
+    title: "I Wanna Be Yours",
+    artist: "Arctic Monkeys",
   },
   {
-    id: "youtube_video_id",
-    title: "All of Me",
-    artist: "John Legend",
+    id: "rWBHbpAo3cg",
+    title: "Rang Jo Lagyo",
+    artist: "Atif Aslam",
+  },
+  {
+    id: "n2dVFdqMYGA",
+    title: "Sahiba",
+    artist: "Aditya Rikhari",
+  },
+  {
+    id: "n2dVFdqMYGA",
+    title: "Paro",
+    artist: "Aditya Rikhari",
+  },
+  {
+    id: "u2ah9tWTkmk",
+    title: "Ordinary",
+    artist: "Alex Warren",
+  },
+  {
+    id: "fldKt9IIUyU",
+    title: "Made in Japan",
+    artist: "Buck Owens",
+  },
+  {
+    id: "0IIJxkDtkHY",
+    title: "HUSN",
+    artist: "Anuv Jain",
+  },
+  {
+    id: "6tFM-QTzmxU",
+    title: "O Meri Laila",
+    artist: "Atif Aslam",
+  },
+  {
+    id: "MBePwJgQpXk",
+    title: "Kalank",
+    artist: "Arijit Singh",
+  },
+  {
+    id: "h9l4YS4h1Vq",
+    title: "Honey Pie",
+    artist: "JAWNY",
+  },
+  {
+    id: "bjHJg_2qPkM",
+    title: "Closer",
+    artist: "The Chainsmokers",
+  },
+  {
+    id: "i96UO8-GFvw",
+    title: "Aaoge Tum Kabhi",
+    artist: "The Local Train",
   },
 ];
 
 export const PLAYLIST_SONGS = [
-  // more songs here
+  {
+    id: "8xg3vE8Ie_E",
+    title: "Love Story",
+    artist: "Taylor Swift",
+  },
+  {
+    id: "uWoYIOcOpwU",
+    title: "Love Me Like You Do",
+    artist: "Ellie Goulding",
+  },
+  {
+    id: "fTrqoVSrw1Y",
+    title: "Perfect",
+    artist: "Ed Sheeran",
+  },
+  {
+    id: "f5-IY_Ja1R",
+    title: "Her",
+    artist: "JVKE",
+  },
+  {
+    id: "KOdPt6MkpiY",
+    title: "Tera Rastaa Chhodoon Na",
+    artist: "Amitabh Bhattacharya, Anusha Mani",
+  },
+  {
+    id: "8CsuLbpBuN4",
+    title: "Fairy Tale",
+    artist: "Alexander Rybak",
+  },
+  {
+    id: "iWTr4Ex22ls",
+    title: "Suroor",
+    artist: "Aditya Rikhari",
+  },
+  {
+    id: "Bt6TmXqRCb4",
+    title: "There's Nothing Holdin' Me Back",
+    artist: "Shawn Mendes",
+  },
+  {
+    id: "M6ZZKNPrSPw",
+    title: "I WANNA BE YOUR SLAVE",
+    artist: "Måneskin",
+  },
+  {
+    id: "n5CxWkohlCs",
+    title: "Samjho Na X Wishes Mashup",
+    artist: "Aditya Rikhari, Talwiinder, Hasan Rasheem",
+  },
+  {
+    id: "-2zKk6ICRf8",
+    title: "Someone You Loved",
+    artist: "Lewis Capaldi",
+  },
+  {
+    id: "wmUJwQNGK3k",
+    title: "JO TUM MERE HO",
+    artist: "Anuv Jain",
+  },
+  {
+    id: "9n4en68ibNI",
+    title: "Tu Jaane Na",
+    artist: "Atif Aslam",
+  },
+  {
+    id: "8mtxEbvzkHs",
+    title: "Матушка",
+    artist: "Татьяна Куртукова",
+  },
 ];
